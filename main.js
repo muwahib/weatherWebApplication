4dbb19c8899cb79936f3234b78a2e528216f3fe8
Muwahib Abdirahman <muwahib2050@gmail.com>
2022-12-28 12:29:43 -0800

    last commit

--- main.js
+++ main.js
@@ -1,49 +1,43 @@
+//declarations
+let searchInput = document.getElementById('city');
+let search = document.getElementById('search');
+let city = document.getElementById('name');
+let temperature = document.getElementById('temperature');
+let feels_like = document.getElementById('feels_like')
+let min_temp = document.getElementById('min')
+let max_temp = document.getElementById('max')
+
 //connection url
 let weatherAPIKey = 'f60f285bbf94aa221d0111239a0a9e34';
-let weatherBase = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid=' + weatherAPIKey;
+let weatherBaseEndpoint  = 'https://api.openweathermap.org/data/2.5/weather?units=metric&appid=' + weatherAPIKey;
 
 // async await
 let weatherCity = async (city) => {
-    let endpoint = weatherBase + '&q=' + city;
+    let endpoint = weatherBaseEndpoint + '&q=' + city;
     let response = await fetch(endpoint);
+
+    if(response.status !== 200){
+        throw new Error('cannot fetch the data');
+    } 
+
     let weather = await response.json();
-    
     return weather;
 }
-
-weatherCity("input")
-.then(weather => getWeather(weather))
-.catch(err => console.log('rejected:', err.message));
-
-
-
-function getWeather(weather){
-    // // read temperature inside json
-    const {name} = weather;
-    const {temp} = weather.main;
-    const {feels_like} = weather.main;
-    const {temp_min} = weather.main;
-    const {temp_max} = weather.main;
-   
-    // //display to my html
-    var div = document.getElementById("div");
-    
-    div.innerHTML += `
-        <h5 class="card-title">City,  ${name}</h5>
-        <h6 class="card-subtitle mb-2">Current temperature: ${temp} °C</h6>
-        <p class="card-text one">Feels Like: ${feels_like} °C</p>
-        <p class="card-text two_min two_max">Max: ${temp_max} °C, Min: ${temp_min} °C</p>
-        <div id="img-container"><img src="https://openweathermap.org/img/wn/04n@2x.png"></div>          
-    `;
-}
-
-var search = document.getElementById('btn');
-search.addEventListener('click', (e) => {
-    e.preventDefault();
-    var searchInput = document.getElementById('city');
-    if(searchInput.value !== ""){
-        var input = JSON.stringify(searchInput);
-        weatherCity("input");
+//search weather
+search.addEventListener('click', async (e) => {
+    if(searchInput !== ""){
+        let weather = await weatherCity(searchInput.value);
+        displayWeather(weather);
+    }else{
+        alert("error")
     }
-   
-});
+})
+
+//display weather inside my html
+let displayWeather = (data) => {
+    city.innerHTML = data.name + ', ' + data.sys.country;
+    temperature.innerHTML = "Current Temperature: " + data.main.temp + "°C";
+    feels_like.innerHTML = "Feels Like: " + data.main.feels_like + "°C";
+    max_temp.innerHTML = "Max Temperature: " + data.main.temp_max + "°C";
+    min_temp.innerHTML = "Min Temperature: " + data.main.temp_min + "°C";
+}
